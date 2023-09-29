4d9c81e6f2d716c72c465b048fb77ab8709fcbb5
SangHui48 <vodlrm123@naver.com>
2023-09-29 16:55:28 +0900

    2023-09-29
    fastapi-react 예시 완성

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
-from typing import Annotated
+from typing import Annotated, List
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from database import SessionLocal, engine
@@ -10,12 +10,17 @@ app = FastAPI()
 
 #CORS
 origins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers=['*']
+
 )
 
 class TransactionBase(BaseModel):
@@ -50,3 +55,8 @@ async def create_transaction(transaction: TransactionBase, db: db_dependency):
     db.commit()
     db.refresh(db_transaction)
     return db_transaction
+
+@app.get("/transactions/", response_model=List[TransactionModel])
+async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions 
